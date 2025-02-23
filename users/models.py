b23656650af527d0b44d3344625697e19b66a27c
alex <kadissa70@gmail.com>
2025-02-23 22:21:37 +0300

    create message model

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import (Column, Integer, String, Boolean, DateTime, ForeignKey,
+                        SmallInteger)
+from sqlalchemy.orm import relationship
 from sqlalchemy.schema import CreateTable
 
 from db import Base
@@ -13,14 +15,15 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    time_stamp = Column(
-        DateTime, default=datetime.datetime.now(tz=datetime.timezone.utc)
-    )
+    timestamp = Column(DateTime,
+                       default=datetime.datetime.now(tz=datetime.timezone.utc))
     user_ip = Column(String)
     user_client = Column(String)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
     is_verified = Column(Boolean, default=False)
+
+    message = relationship('Message', back_populates='user')
 
     def __repr__(self):
         return (f"User(id={self.id}, username={self.username}, "
@@ -32,5 +35,28 @@ class User(Base):
         return self.username
 
 
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True, index=True)
+    text = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    timestamp = Column(DateTime,
+                       default=datetime.datetime.now(tz=datetime.timezone.utc))
+    to_user = Column(SmallInteger)
+    from_user = Column(SmallInteger)
+    is_read = Column(Boolean, default=False)
+    is_received = Column(Boolean, default=False)
+    user = relationship(User, back_populates='message')
+
+    def __repr__(self):
+        return (f"Message(id={self.id}, text={self.text}, "
+                f"user_id={self.user})")
+
+    def __str__(self):
+        return self.text
+
+
 if __name__ == '__main__':
     print(CreateTable(User.__table__))
+    print(CreateTable(Message.__table__))
